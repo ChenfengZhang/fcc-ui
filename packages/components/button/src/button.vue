a7a811a9f929fb04598a8c692bfa2e8a6fc03477
zhang.chenfeng <3225333435@qq.com>
2024-10-24 10:01:25 +0800

    fix: BEM命名规范的添加

---
+++ packages/components/button/src/button.vue
@@ -0,0 +1,8 @@
+<template>
+    <button :class="bem.b()">测试按钮</button>
+  </template>
+  
+  <script lang="ts" setup>
+  import { useNamespace } from '@fcc-ui/hooks'
+  const bem = useNamespace('button')
+  </script>
--- play/src/App.vue
+++ play/src/App.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import VButton from '@fcc-ui/components/button'
+import '@fcc-ui/theme-chalk/src/index.scss'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+   <div>
+    <v-button></v-button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
